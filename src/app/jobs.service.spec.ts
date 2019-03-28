1042632fc3ae3e83a14690c587ac140882f931b7
Aneesh Kallarakkal <kallarakkal@hotmail>
2019-03-28 13:26:34 +0000

    add test for job service

--- src/app/jobs.service.spec.ts
+++ src/app/jobs.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JobsService } from './jobs.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('JobsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClientSpy: { get: jasmine.Spy };
+  let service: JobsService;
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new JobsService(httpClientSpy as unknown as HttpClient);
+  });
 
   it('should be created', () => {
-    const service: JobsService = TestBed.get(JobsService);
     expect(service).toBeTruthy();
   });
+  it('should make request to get data', () => {
+    service.data();
+    expect(httpClientSpy.get).toHaveBeenCalled();
+    expect(httpClientSpy.get).toHaveBeenCalledWith('/api/data')
+  })
 });
