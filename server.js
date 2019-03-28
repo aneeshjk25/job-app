7f7f99dcdeccc8ffb13adada31c4a7421c3afe9b
Aneesh Kallarakkal <kallarakkal@hotmail>
2019-03-28 13:47:37 +0000

    heroku changes

---
+++ server.js
@@ -0,0 +1,20 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+var proxy = require('http-proxy-middleware')
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/advertisement-report'));
+app.use('/api', proxy({
+    target: '', changeOrigin: true, pathRewrite: {
+        "^/api": ""
+    }
+}))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/job-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080); 
